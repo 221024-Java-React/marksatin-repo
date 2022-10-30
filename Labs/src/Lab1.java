d93da08b0b84df4754213828fb2c13b7032cefca
msatin1 <mark@marksatin.com>
2022-10-30 13:29:13 -0400

    added Lab1

---
+++ Labs/src/Lab1.java
@@ -0,0 +1,22 @@
+
+public class Lab1 {
+
+	static boolean boo = true;
+	static byte maxByte = 127;
+	static short minShort = -32768;
+	static int maxInt = 2147483647;
+	static long javaFlex = 3000000000L;
+	static float pie = 3.14159f;
+	static double doubleYum = 6.28318;
+	
+	public static void main(String[] args) {
+		System.out.println("The value of boo is " + boo);
+		System.out.println("The value of maxByte is " + maxByte);
+		System.out.println("The value of minShort is " + minShort);
+		System.out.println("The value of maxInt is " + maxInt);
+		System.out.println("The value of javaFlex is " + javaFlex);
+		System.out.println("The value of pie is " + pie);
+		System.out.println("The value of doubleYum is " + doubleYum);
+	}
+
+}
