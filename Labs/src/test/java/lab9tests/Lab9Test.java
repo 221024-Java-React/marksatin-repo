c039f5861b5ab5f024dac98dd2e51ff40d53296c
msatin1 <mark@marksatin.com>
2022-11-03 22:39:03 -0400

    init commit lab 9 tests

--- Labs/src/test/java/lab9tests/Lab9Test.java
+++ Labs/src/test/java/lab9tests/Lab9Test.java
@@ -1,21 +1,72 @@
-package lab9test;
+package lab9tests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import lab9.Lab9;
 
 public class Lab9Test {
 
-	private Lab9 l9test = new Lab9();
-	private char[] charArray = new char[]{'A', 'B', 'B', 'F', 'D'};
-	private int[] expectedArray = new int[]{5000, 2500, 2500, 0, 500};
+	private static Lab9 l9;
+	private char[] charArray = new char[]{'A', 'B', 'C', 'D', 'F'};
+	private char[] wrongCharsArr = new char[] {'Z', 'P', 'E', 'L'};
+	ArrayList<Integer> actualList = new ArrayList<Integer>();
+
 	
+	@BeforeClass
+	public static void setUpNewLab9() {
+		l9 = new Lab9();
+	}
+	
+	// Will PASS
 	@Test
-	public void testResultA() {
-		int[] actualArray = l9test.calculateBonuses(charArray);
+	public void lab9Test1() {
+		ArrayList<Integer> expectedList = new ArrayList<Integer>(List.of(5000, 2500, 1000, 500, 0));
 		
-		assertEquals("dd", expectedArray, actualArray);
+		for (int i : l9.calculateBonuses(charArray)) {
+			actualList.add(i);
+		}
+		
+		assertEquals("chars match correct int values", expectedList, actualList);
+	}
+	
+	// Will PASS
+	@Test
+	public void lab9Test2() {
+	ArrayList<Integer> expectedList = new ArrayList<Integer>(List.of(0, 0, 0, 0));
+		
+		for (int i : l9.calculateBonuses(wrongCharsArr)) {
+			actualList.add(i);
+		}
+		
+		assertEquals("chars match correct int values", expectedList, actualList);
+	}
+	
+	// Will FAIL
+	@Test
+	public void lab9Test3() {
+		int[] intArray = l9.calculateBonuses(charArray);
+		
+		assertEquals("char array input and int array output are same object", charArray, intArray);
+	}
+	
+	// Will FAIL
+	@Test
+	public void lab9Test4() {
+		char[] emptyCharArr = new char[5];
+		
+		for (int i : l9.calculateBonuses(emptyCharArr)) {
+			actualList.add(i);
+		}
+		
+		String expectedException = "The array is empty and the calculateBonuses() method cannot be executed.";
+		
+		System.out.println(actualList);
+		assertEquals("an empty char away will throw an error", expectedException, emptyCharArr);
 	}
 }
